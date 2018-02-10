57084f9abee6ed52a876feab437c7a71fa7ceb09
Jose Medina <fermed28@gmail.com>
2018-02-10 11:06:19 -0500

    modiffied orde json format

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
-namespace :api, defaults: { format: "json"}do
-  namespace :v1 do
+namespace :api, defaults:  do
+  namespace :v1  { format: "json"} do
     resources :products
 
   end
