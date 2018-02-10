616dcbf1830322e9daa6bc6892a2e00b5d65a6c6
Jose Medina <fermed28@gmail.com>
2018-02-10 17:55:39 -0500

    set status code 200 on update response

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -20,7 +20,7 @@ class Api::V1::ProductsController < ApplicationController
   		product= Product.find(params[:id])
 
   		if product.update(product_params)
-  			render json: product, status: 201 
+  			render json: product, status: 200 
   		else
   			render json: {errors: product.errors}, status: 422
   		end
