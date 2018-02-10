96fc9e3cfcbf0a40007d3512628664b2a79a31a5
Jose Medina <fermed28@gmail.com>
2018-02-10 16:37:35 -0500

    add create product, response with status code 201/422 respectly

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,26 @@
-class Api::V1::ProductsController < ApplicationController
+class Api::V1::ProductsController < ApplicationController	 
+
 	def index
-    @products = Product.all  
-    render json: @products.as_json()
-  end
+    	@products = Product.all  
+    	render json: @products.as_json()
+  	end
+
+  	def create	
+
+  		p 'Aqui de nuevo'
+  		product= Product.new(product_params)
+  		if product.save
+  			render json: product, status: 201 
+  		else
+  			render json: {errors: product.errors}, status: 422
+  		end 				
+  		
+  	end
+  	
+  	private
+
+  	def product_params
+  		params.require(:product).permit(:name, :price)
+  	end
+
 end
